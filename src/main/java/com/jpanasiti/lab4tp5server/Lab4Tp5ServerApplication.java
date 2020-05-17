d0ef98ee2f687a5e7b08da3edca337614c9a1d99
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-05-17 01:53:39 -0300

    Inicio del TP05, configuración del .properties

---
+++ src/main/java/com/jpanasiti/lab4tp5server/Lab4Tp5ServerApplication.java
@@ -0,0 +1,13 @@
+package com.jpanasiti.lab4tp5server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Tp5ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4Tp5ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpanasiti/lab4tp5server/Lab4Tp5ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpanasiti.lab4tp5server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4Tp5ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
